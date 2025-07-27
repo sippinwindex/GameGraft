5c39069febe0b916335c24c34649c6ac0d34f670
Juan Fernandez <stormblazart@gmail.com>
2025-07-27 20:22:09 +0000

    feat: Consolidate Steam integration
    - Remove duplicate Steam service implementations (steamServiceProxy, steamService.backup)
    - Remove redundant useSteam hook - migrate to useSteamConnection
    - Remove duplicate SteamManager component - use SteamManagerCORS
    - Update all imports and exports to use consolidated implementations
    - Fix Steam/index.js barrel export
    - Reduce Steam-related files from 6 to 4 core files
    - Eliminate developer confusion and maintenance overhead

--- src/front/components/Steam/index.js
+++ src/front/components/Steam/index.js
@@ -1,7 +1,7 @@
 // src/front/components/Steam/index.js
 // Barrel export for all Steam components
 
-export { default as SteamManager } from '../SteamManager';
+export { default as SteamManager } from '../SteamManagerCORS';
 export { default as EnhancedSteamFeatures } from './EnhancedSteamFeatures';
 export { default as SteamComponents } from './SteamComponents';
 
